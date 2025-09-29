7c0a1fde80b45d54d35ad163aff417e7c7248dfe
小涛daniel <859926750@qq.com>
2025-09-29 16:06:43 +0800

    引入项目

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+
+import "./assets/tailwind.min.css";
+import "./assets/all.min.css";
 
 createApp(App).mount('#app')
